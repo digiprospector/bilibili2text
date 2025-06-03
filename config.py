06658589f698418f1a7f3d30e132428bb45c6abd
digiprospector <digiprospector@protonmail.com>
2025-06-04 00:29:21 +0800

    init

---
+++ config.py
@@ -0,0 +1,6 @@
+
+config = {
+    "downloader": "C:\\software\\BilibiliDownloader\\src\\main.py",
+    "stt": "C:\\software\\Faster-Whisper-XXL\\faster-whisper-xxl.exe",
+    "temp_dir": "C:\\temp"
+}
